4d572771ea2330611259ebb5b27fc0251688bc81
Rémi Granjon <remi.granjon@outlook.fr>
2023-05-09 10:15:07 +0200

    fin Etape 2 : Parcours utilisateur

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://remigranjon:wlQiVxaeVtJoJ787@cluster0.2ql35w1.mongodb.net/?retryWrites=true&w=majority',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>console.log('Connexion à MongoDB réussie!'))
+  .catch(()=> console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
